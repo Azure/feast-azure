59459d02a3adf4546265fe0fb3d997c4a63b64ba
cbtham <tham90cb@gmail.com>
2022-01-20 09:55:19 +0800

    attempt to update to latest 0.17

--- provider/sdk/setup.py
+++ provider/sdk/setup.py
@@ -21,7 +21,7 @@ with open(README_FILE, "r") as f:
 setup(
     name="feast-azure-provider",
     author="Microsoft",
-    version="0.2.2",
+    version="0.2.29",
     description="A Feast Azure Provider",
     URL="https://github.com/Azure/feast-azure",
     long_description=LONG_DESCRIPTION,
@@ -29,7 +29,7 @@ setup(
     python_requires=">=3.7.0",
     packages=find_packages(exclude=("tests",)),
     install_requires=[
-        "feast[redis]==0.15.1",
+        "feast[redis]==0.17.0",
         "azure-storage-blob>=0.37.0",
         "azure-identity>=1.6.1",
         "SQLAlchemy>=1.4.19",
