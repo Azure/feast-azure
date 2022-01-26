ed521ef804896e0f9f832259bd50f73ce875212e
Richin Jain <rijai@microsoft.com>
2022-01-26 10:14:42 -0800

    Adding note mentioning our only supported version for Kubernetes is 1.21.7 and reverting back earlier helm changes as latest helm chart does not work with our deployment script

--- cluster/setup/installfeast.sh
+++ cluster/setup/installfeast.sh
@@ -80,8 +80,7 @@ az aks install-cli
 az aks get-credentials --resource-group $resourceGroup --name $aksName --overwrite-existing
 
 echo "INFO: Add feast charts"
-helm repo add feast-charts https://feast-helm-charts.storage.googleapis.com
-helm repo update
+helm repo add feast-charts https://feast-charts.storage.googleapis.com
 
 echo "INFO: Install feast"
 kubectl create secret generic feast-postgresql --from-literal=postgresql-password="${sqlPassword}"
