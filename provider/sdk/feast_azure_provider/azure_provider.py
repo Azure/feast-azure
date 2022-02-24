7db2c69e8009efb768a0d0d77acaaa599a9d1b02
cbtham <MY_NAME@example.com>
2022-02-24 11:52:15 +0800

    fix join_key & missing method

--- provider/sdk/feast_azure_provider/azure_provider.py
+++ provider/sdk/feast_azure_provider/azure_provider.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 
 import pandas
+import pyarrow as pa
 from tqdm import tqdm
 
 from feast.entity import Entity
@@ -89,6 +90,21 @@ class AzureProvider(Provider):
             result = self.online_store.online_read(config, table, entity_keys, requested_features)
         return result
 
+    def ingest_df(
+        self, feature_view: FeatureView, entities: List[Entity], df: pandas.DataFrame,
+    ):
+        table = pa.Table.from_pandas(df)
+
+        if feature_view.batch_source.field_mapping is not None:
+            table = _run_field_mapping(table, feature_view.batch_source.field_mapping)
+
+        join_keys = {entity.join_key: entity.value_type for entity in entities}
+        rows_to_write = _convert_arrow_to_proto(table, feature_view, join_keys)
+
+        self.online_write_batch(
+            self.repo_config, feature_view, rows_to_write, progress=None
+        )
+
     def materialize_single_feature_view(
         self,
         config: RepoConfig,
@@ -126,7 +142,7 @@ class AzureProvider(Provider):
         if feature_view.batch_source.field_mapping is not None:
             table = _run_field_mapping(table, feature_view.batch_source.field_mapping)
 
-        join_keys = [entity.join_key for entity in entities]
+        join_keys = {entity.join_key: entity.value_type for entity in entities}
         
         with tqdm_builder(table.num_rows) as pbar:
             for batch in table.to_batches(DEFAULT_BATCH_SIZE):
